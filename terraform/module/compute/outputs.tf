cda33ab5056bcc9cd71838664d81d39bf33cb333
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-17 22:10:25 +0530

    Update outputs.tf

--- terraform/module/compute/outputs.tf
+++ terraform/module/compute/outputs.tf
@@ -1,41 +1,33 @@
 output "eks_cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = module.eks.cluster_name
+  value = module.eks.cluster_name
 }
 
 output "eks_cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks.cluster_id
+  value = module.eks.cluster_id
 }
 
 output "eks_cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  value = module.eks.cluster_endpoint
 }
 
 output "eks_cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
+  value = module.eks.cluster_certificate_authority_data
 }
 
 output "eks_cluster_arn" {
-  description = "ARN of the EKS cluster"
-  value       = module.eks.cluster_arn
+  value = module.eks.cluster_arn
 }
 
 output "eks_oidc_provider_arn" {
-  description = "ARN of the OIDC provider"
-  value       = module.eks.oidc_provider_arn
+  value = module.eks.oidc_provider_arn
 }
 
 output "eks_managed_node_group_names" {
-  description = "List of EKS managed node group names"
-  value       = keys(module.eks.eks_managed_node_groups)
+  value = keys(module.eks.eks_managed_node_groups)
 }
 
 output "eks_managed_node_group_arns" {
-  description = "List of EKS managed node group ARNs"
   value = [
-    for ng in values(module.eks.eks_managed_node_groups) : ng.arn
+    for ng in values(module.eks.eks_managed_node_groups) : try(ng.resources[0].arn, "")
   ]
 }
