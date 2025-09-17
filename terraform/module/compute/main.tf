1ffc380d9408fbfe5b16681ef83b8b7fbcbb82b3
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-17 20:28:32 +0530

    Update main.tf

--- terraform/module/compute/main.tf
+++ terraform/module/compute/main.tf
@@ -1,22 +1,25 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"  # Specify a compatible version
+
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
-  subnets         = data.terraform_remote_state.network.outputs.private_subnet_ids
+  subnet_ids      = data.terraform_remote_state.network.outputs.private_subnet_ids
   vpc_id          = data.terraform_remote_state.network.outputs.vpc_id
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
-  node_groups = {
+  # Use eks_managed_node_groups instead of node_groups
+  eks_managed_node_groups = {
     for ng_name, ng in var.node_groups :
     ng_name => {
-      desired_capacity = ng.desired_size
-      max_capacity     = ng.max_size
-      min_capacity     = ng.min_size
+      desired_size = ng.desired_size
+      max_size     = ng.max_size
+      min_size     = ng.min_size
 
-      instance_type = ng.instance_type
-      subnet_ids    = data.terraform_remote_state.network.outputs.private_subnet_ids
+      instance_types = [ng.instance_type]
+      subnet_ids     = data.terraform_remote_state.network.outputs.private_subnet_ids
 
       key_name = var.key_name
     }
