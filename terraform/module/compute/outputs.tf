a5ae3e241d6ce04d130e30427ff559a57f012c82
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-17 20:41:58 +0530

    Update outputs.tf

--- terraform/module/compute/outputs.tf
+++ terraform/module/compute/outputs.tf
@@ -1,15 +1,39 @@
 output "eks_cluster_name" {
-  value = module.eks.cluster_id
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
 }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_kubeconfig" {
-  value = module.eks.kubeconfig
+output "eks_cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
 }
 
-output "node_group_names" {
-  value = keys(module.eks.node_groups)
+output "eks_cluster_arn" {
+  description = "ARN of the EKS cluster"
+  value       = module.eks.cluster_arn
+}
+
+output "eks_oidc_provider_arn" {
+  description = "ARN of the OIDC provider"
+  value       = module.eks.oidc_provider_arn
+}
+
+output "eks_managed_node_group_names" {
+  description = "List of EKS managed node group names"
+  value       = keys(module.eks.eks_managed_node_groups)
+}
+
+output "eks_managed_node_group_arns" {
+  description = "List of EKS managed node group ARNs"
+  value       = values(module.eks.eks_managed_node_groups)[*].arn
 }
