ae938ac7f6fa0beef1e59511c9bd6a0581cdfa8a
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-17 20:38:24 +0530

    Update variables.tf

--- terraform/module/compute/variables.tf
+++ terraform/module/compute/variables.tf
@@ -1,32 +1,39 @@
 variable "aws_region" {
-  type = string
+  description = "AWS region where resources will be deployed"
+  type        = string
 }
 
 variable "environment" {
-  type = string
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
 }
 
 variable "network_s3_bucket" {
-  type = string
+  description = "S3 bucket storing the remote state of the network module"
+  type        = string
 }
 
 variable "network_s3_key" {
-  type = string
+  description = "S3 key (path) of the remote state file for the network module"
+  type        = string
 }
 
 variable "cluster_name" {
-  type    = string
+  description = "EKS cluster name"
+  type        = string
 }
 
 variable "key_name" {
-  type = string
+  description = "Name of an existing EC2 key pair to enable SSH access to nodes"
+  type        = string
 }
 
 variable "node_groups" {
+  description = "Map of managed node groups"
   type = map(object({
-    instance_type = string
-    desired_size  = number
-    max_size      = number
-    min_size      = number
+    instance_types = list(string)
+    desired_size   = number
+    max_size       = number
+    min_size       = number
   }))
 }
