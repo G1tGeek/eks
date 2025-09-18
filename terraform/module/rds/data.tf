ee974e7120401f857e73a9b3b0ae6772f6382f69
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-18 14:29:05 +0530

    Update data.tf

--- terraform/module/rds/data.tf
+++ terraform/module/rds/data.tf
@@ -1,4 +1,3 @@
-
 data "terraform_remote_state" "network" {
   backend = "s3"
 
@@ -7,4 +6,13 @@ data "terraform_remote_state" "network" {
     key    = var.network_remote_state_key
     region = var.network_remote_state_region
   }
+}
+
+data "aws_secretsmanager_secret_version" "rds" {
+  count     = var.rds_secret_name != "" ? 1 : 0
+  secret_id = var.rds_secret_name
+}
+
+locals {
+  rds_credentials = var.rds_secret_name != "" ? jsondecode(data.aws_secretsmanager_secret_version.rds[0].secret_string) : {}
 }
