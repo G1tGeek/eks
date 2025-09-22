91d377ec3c58450ebb79d8109778d57b74593d8d
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-22 12:07:23 +0530

    Update main.tf

--- terraform/module/ecr/main.tf
+++ terraform/module/ecr/main.tf
@@ -32,7 +32,7 @@ resource "aws_ecr_repository" "this" {
   for_each = toset(var.repositories)
 
   name                 = each.value
-  image_tag_mutability = "MUTABLE"  # Keep mutable for CI/CD, skipping CKV_AWS_51
+  image_tag_mutability = "IMMUTABLE" 
 
   image_scanning_configuration {
     scan_on_push = true
