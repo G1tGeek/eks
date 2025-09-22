921caf80b4ee94bd40ab0289d92d1d91212b5530
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-22 12:09:09 +0530

    Update main.tf

--- terraform/wrapper/ecr/main.tf
+++ terraform/wrapper/ecr/main.tf
@@ -1,5 +1,8 @@
 module "ecr" {
   source = "git::https://github.com/G1tGeek/eks.git//terraform/module/ecr?ref=main"
+  
+  # checkov:skip=CKV_TF_1: Skipping commit hash pinning check
+  # checkov:skip=CKV_TF_2: Skipping tag pinning check
 
   environment = "eks"
 
