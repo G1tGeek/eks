b1dd8222a9524827b8d8c5cbd607d21238da17cf
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-22 12:05:08 +0530

    Update main.tf

--- terraform/module/ecr/main.tf
+++ terraform/module/ecr/main.tf
@@ -1,19 +1,44 @@
+########################################################
+# KMS Key for ECR encryption
+########################################################
 resource "aws_kms_key" "ecr_key" {
   description             = "KMS key for ECR repositories"
   deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid       = "Allow administration of the key"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = [
+          "kms:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
+########################################################
+# ECR Repositories
+########################################################
 resource "aws_ecr_repository" "this" {
   for_each = toset(var.repositories)
 
   name                 = each.value
-  image_tag_mutability = "MUTABLE"   # keep mutable, skipping Checkov CKV_AWS_51
+  image_tag_mutability = "MUTABLE"  # Keep mutable for CI/CD, skipping CKV_AWS_51
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
-  encryption_configuration {           # enable KMS encryption
+  encryption_configuration {
     encryption_type = "KMS"
     kms_key         = aws_kms_key.ecr_key.arn
   }
