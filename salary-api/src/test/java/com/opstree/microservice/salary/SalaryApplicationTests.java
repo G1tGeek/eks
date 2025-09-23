00b126b4bce6e1c817b0380afc09141453433345
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-09-23 19:18:31 +0530

    Add files via upload

---
+++ salary-api/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.opstree.microservice.salary;
+
+import org.junit.jupiter.api.Test;
+
+
+class SalaryApplicationTests {
+
+
+        @Test
+void basicTest(){
+        assert true;
+}
+
+
+}
